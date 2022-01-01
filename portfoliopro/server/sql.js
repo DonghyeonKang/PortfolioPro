5daba8059703f514dfce34b13d0ab45db562a6db
Donghyeon Kang <86303312+DonghyeonKang@users.noreply.github.com>
2022-01-01 20:40:21 +0900

    Move server

---
+++ portfoliopro/server/sql.js
@@ -0,0 +1,14 @@
+module.exports = {
+    folioList: {    // folio img, name
+        query: `select * from portfolioes`
+    },
+    ProjectDetail: {    // folio detail
+        query: `select * from portfolioes`
+    },
+    ProfileImage: {  // 프로필의 이미지
+        query: `select * from portfolioes`
+    },
+    ProjectImage: {  // 프로젝트의 이미지
+        query: `select * from portfolioes`
+    }
+}
