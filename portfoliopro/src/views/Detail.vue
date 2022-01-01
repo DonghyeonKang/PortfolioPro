692cfb18750b45856eb4d49dcd65e1b7dd031bb3
Donghyeon Kang <86303312+DonghyeonKang@users.noreply.github.com>
2022-01-01 22:43:38 +0900

    Update Header

--- portfoliopro/src/views/Detail.vue
+++ portfoliopro/src/views/Detail.vue
@@ -1,15 +1,32 @@
 <template>
+  <h2>Profile</h2>
   <section class="profile">
-    <img :src="profileImage.image_path" alt="profile_img">
-    <p></p>
+    <img src="https://w.namu.la/s/48a178fbd427b8dddc275cf878725e03d8499105f13e57c7ff889ef1cba3895d813b88696242cebb4f56d28ae2c93fbdef1f61731db76fc27f9070a8fceef4652016fbd8c0a144273d70a69c7dfff6d9" alt="profile_img">
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores ab eaque odio placeat quia ad vitae. Vero consectetur, error repellat dolor quasi rem? Provident accusantium voluptatum vel, fugiat nesciunt rerum?</p>
   </section>
-  <section class="project">
-    <div class="project_inner" :key="i" v-for="(image, i) in projectImage">
-      <img :src="image.image_path" alt="profile_img">
-      <p></p>
+  <h2>Projects</h2>
+  <section class="projects">
+    <div class="project">
+      <img src="https://w.namu.la/s/48a178fbd427b8dddc275cf878725e03d8499105f13e57c7ff889ef1cba3895d813b88696242cebb4f56d28ae2c93fbdef1f61731db76fc27f9070a8fceef4652016fbd8c0a144273d70a69c7dfff6d9" alt="profile_img">
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Labore dignissimos distinctio minus aut corrupti ex hic rerum voluptates, voluptatibus ullam nesciunt quia in? Iste commodi blanditiis aliquid ea cum voluptates?</p>
     </div>
   </section>
 </template>
+
+<style scoped>
+.profile {
+  display: flex;
+}
+.profile img {
+  width: 10rem;
+}
+.project {
+  display: flex;
+}
+.project img{
+  width: 10rem;
+}
+</style>
 
 <script>
 export default {
--- portfoliopro/src/views/Home.vue
+++ portfoliopro/src/views/Home.vue
@@ -1,15 +1,15 @@
 <template>
   <div class="wrap">
-    <a @click="goToDetail(folio.folio_id)" :key="i" v-for="(folio, i) in folioList">
+    <div class="folio_box" @click="goToDetail(folio.folio_id)" :key="i" v-for="(folio, i) in folioList">
       <img :src="folio.path" alt="folioImg">
       <h3>{{folio.folio_name}}</h3>
-    </a>
+    </div>
   </div>
 </template>
 
 <style scoped>
-.folio_list {
-    max-width: 15rem;
+.folio_box {
+    max-width: 10rem;
     width: 100%;
     min-width: 2rem;
     height: 15rem;
@@ -17,9 +17,9 @@
     margin: 0 2rem 0 2rem;
 }
 
-.folio_img {
-    width: 100%;
-    height: 80%;
+.folio_box img {
+  width: 95%;
+  margin-top: 1rem;
 }
 
 .wrap a {
